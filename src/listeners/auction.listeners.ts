b8c2bbd8306ddd0ed6adfe37df8a444961f86bf1
diegomated1 <diegocardwr@gmail.com>
2023-03-30 14:55:18 -0500

    create, delete bidd and auctions

--- src/listeners/auction.listeners.ts
+++ src/listeners/auction.listeners.ts
@@ -6,13 +6,13 @@ import AuctionModel from "../models/auction.models.js";
 
 /*
 
-GET     /auction/cards
-POST    /auction/cards
+#GET     /auction/cards
+#POST    /auction/cards
 
-DELETE  /auction/cards/:id_card
-GET     /auction/cards/:id_card/auctions
-POST    /auction/cards/:id_card/auctions
-DELETE  /auction/cards/:id_card/auctions
+#DELETE  /auction/cards/:id_card
+#GET     /auction/cards/:id_card/auctions
+#POST    /auction/cards/:id_card/auctions
+#DELETE  /auction/cards/:id_card/auctions
 
 */
 
@@ -38,14 +38,52 @@ export default class AuctionListeners{
                 min_coins: auction.min_coins,
                 insta_win: auction.insta_win
             });
-            this.socket.emit('post:auctions', _auction);
+            this.io.emit('post:auctions', _auction);
         }catch(error){
-            this.socket.emit('post:auctions');
+            //this.socket.emit('post:auctions');
+        }
+    }
+
+    private async deleteAuction(id_auction:string){
+        try{
+            await AuctionModel.findByIdAndDelete(id_auction);
+            this.io.emit('delete:auctions', id_auction);
+        }catch(error){
+            //this.socket.emit('delete:auction');
+        }
+    }
+
+    private async createBidd(id_auction:string, id_user:string, coins:number, cards:string[]){
+        try{
+            const auction = await AuctionModel.findById(id_auction);
+            if(auction){
+                const bidd = {id_user, coins, cards, time: new Date()};
+                auction.bidders.push(bidd);
+                await auction.save();
+                this.io.emit('post:auctions:bidd', id_auction, bidd);
+            }
+        }catch(error){
+            //this.socket.emit('post:auctions:bidd');
+        }
+    }
+
+    private async deleteBidd(id_auction:string, id_user:string){
+        try{
+            await AuctionModel.updateOne(
+                { _id: id_auction },
+                { $pull: { bidders: { id_user } } }
+            );
+            this.io.emit('delete:auctions:bidd', id_auction, id_user);
+        }catch(error){
+            //this.socket.emit('delete:auctions:bidd');
         }
     }
 
     listeners(){
         this.socket.on('post:auctions', this.createAuction);
+        this.socket.on('delete:auctions', this.deleteAuction);
+        this.socket.on('post:auctions:bidd', this.createBidd);
+        this.socket.on('delete:auctions:bidd', this.deleteBidd);
     }
 
 }
